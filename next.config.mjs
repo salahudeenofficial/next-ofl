4e18ad5806ac3a4db3d5c026d33be29fa3ba6c7d
salahudeenofficial <63786828+salahudeenofficial@users.noreply.github.com>
2024-08-25 13:04:13 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@
 
 const nextConfig = {
   output: "export",
-  basePath: "/next-ofl/site", // Replace with your GitHub repository name
+  basePath: "/next-ofl", // Replace with your GitHub repository name
   images: {
     unoptimized: true, // Disable image optimization for GitHub Pages
   },
