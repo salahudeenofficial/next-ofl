8d0a82980810c5e9cf50e369fd7d5147c8c92cae
salahudeenofficial <63786828+salahudeenofficial@users.noreply.github.com>
2024-08-25 16:42:29 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ const nextConfig = {
   output: "export",
   reactStrictMode: true,
   images: {
-    unoptimized: true,  // Disable server-side image optimization
+    unoptimized: false,  // Disable server-side image optimization
   },
   basePath: '/next-ofl',
   assetPrefix: '/next-ofl',
